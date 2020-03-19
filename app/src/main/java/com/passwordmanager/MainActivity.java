7a4dff5fcdbc1af8fef4e23b06ddbb82ef3a2808
Prasanta Barman <prasanta.2001.7@gmail.com>
2020-03-19 11:39:09 +0530

    added simple file write operation

--- app/src/main/java/com/passwordmanager/MainActivity.java
+++ app/src/main/java/com/passwordmanager/MainActivity.java
@@ -1,14 +1,41 @@
 package com.passwordmanager;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String filename = "passwords";
+        String data = "data";
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput(filename, Context.MODE_APPEND));
+            Log.d(TAG, "file created: " + filename);
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (Exception e) {
+            Log.d(TAG, "error: ");
+            e.printStackTrace();
+        }
+
+
+        Log.d(TAG, "onCreate: ");
+
+
     }
+
+
 }
