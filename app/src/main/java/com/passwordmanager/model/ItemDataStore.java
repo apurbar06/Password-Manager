072436063d6bb7085e8e3b7c91ecbf86682e3e7d
Apurba Roy <apurbar06@gmail.com>
2020-03-19 11:17:34 +0530

    added data class

---
+++ app/src/main/java/com/passwordmanager/model/ItemDataStore.java
@@ -0,0 +1,49 @@
+package com.passwordmanager.model;
+
+public class ItemDataStore {
+    private String title;
+    private String username;
+    private String password;
+    private int id;
+
+    public ItemDataStore(int id, String title, String username, String password){
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
